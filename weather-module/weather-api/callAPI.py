e59720039f02dde176253401d09dc14517e2067b
JoaoCioffi <joaorcioffi@gmail.com>
2023-03-10 13:56:59 -0300

    updated weather module

--- weather-module/weather-api/callAPI.py
+++ weather-module/weather-api/callAPI.py
@@ -2,22 +2,26 @@ import env
 import requests
 import json
 
-def callAPI():
+def callAPI(
+        LAT=-27.670279,
+        LONG=-48.552502):
+    """
+    Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, 
+    Latitude/Longitude (decimal degree) or city name
+    """
 
     # loading environment
     constants = env.environment()
-    
+
     # creating URL
-    endpoint = f"/metar/{constants['icao']}/decoded"
+    endpoint = f"/v1/forecast.json?key={constants['accessKey']}&q={LAT},{LONG}&days=1&aqi=yes&alerts=yes"
     url = constants["protocol"] + constants["baseUrl"] + endpoint
 
     # calling API
     method = "GET"
-    headers = {"X-API-Key":constants["accessKey"]}
     response = requests.request(
         method,
-        url,
-        headers=headers
+        url
     )
     createdObject = json.loads(response.text)
     dumppedObject = json.dumps(createdObject,indent=4,ensure_ascii=False)
---
+++ weather-module/weather-api/env.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+def environment(dotEnv=load_dotenv()):
+    constants={
+        "protocol":"https://",
+        "baseUrl":os.getenv("BASE_URL"),
+        "accessKey":os.getenv("API_KEY")
+    }
+
+    return constants
