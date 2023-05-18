2fff6f1c1826b8c84acc69a59949a3250e56f3a0
JoaoCioffi <joaorcioffi@gmail.com>
2023-05-18 13:39:17 -0300

    rosdep install + colcon build for py_pubsub

--- ROS/sandbox/auto-gen.sh
+++ ROS/sandbox/auto-gen.sh
@@ -1,7 +1,13 @@
 source /opt/ros/humble/setup.bash
+rosdep update
 mkdir -p ros2_ws/src
 cd ros2_ws/src
 git clone https://github.com/ros/ros_tutorials.git -b humble-devel
 cd ..
 rosdep install -i --from-path src --rosdistro humble -y
 colcon build
+cd src
+ros2 pkg create --build-type ament_python py_pubsub
+cd py_pubsub/py_pubsub
+wget https://raw.githubusercontent.com/ros2/examples/humble/rclpy/topics/minimal_publisher/examples_rclpy_minimal_publisher/publisher_member_function.py
+wget https://raw.githubusercontent.com/ros2/examples/humble/rclpy/topics/minimal_subscriber/examples_rclpy_minimal_subscriber/subscriber_member_function.py
---
+++ ROS/sandbox/ros2_ws/build/py_pubsub/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/noetic-sierra/Downloads/SIMUA/ROS/sandbox/ros2_ws/install/py_pubsub'
